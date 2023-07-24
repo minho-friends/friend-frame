9420a7d365cae1ffcc7ca8c759746d57ea9e54af
Minho Ryang <minhoryang@gmail.com>
2023-07-25 01:51:51 +0200

    Greeting friend-frame

---
+++ src/env.d.ts
@@ -0,0 +1,4 @@
+export interface Env {
+  TARGET_HOST: string;
+  TARGET_KEYWORD: string;
+}
---
+++ src/index.ts
@@ -0,0 +1,75 @@
+import type { Env } from "./env";
+
+class ElementRemover implements HTMLRewriterElementContentHandlers {
+  element(element: Element) {
+    element.remove();
+  }
+}
+const elementRemover = new ElementRemover();
+
+const KeywordRemoverButInZeroCopy = (keyword /* FIXME: keywordHopelyInFirstChunkSoNotCorruptingElement */: string): HTMLRewriterElementContentHandlers => {
+  // NOTE: https://developers.cloudflare.com/workers/runtime-apis/html-rewriter/#text-chunks
+  // NOTE: beware in future use (FIXMEs)
+
+  let _foundPreviously = false;
+  class _KeywordRemoverButInZeroCopy implements HTMLRewriterElementContentHandlers {
+    text(element: Text) {
+      if (!keyword) return;
+      if (_foundPreviously || element.text.includes(keyword) /* FIXME: what-if the keyword splitted in first chunk? */) {
+        element.remove();
+        if (element.lastInTextNode /* NOTE: search done? */) {
+          _foundPreviously = false;
+        } else {
+          _foundPreviously = true;
+        }
+      }
+    }
+  }
+  return new _KeywordRemoverButInZeroCopy();
+};
+
+export default {
+  async fetch(
+    request: Request,
+    env: Env,
+    ctx: ExecutionContext
+  ): Promise<Response> {
+    if (!env.TARGET_HOST) {
+      return new Response('TARGET_HOST required', { status: 503 });
+    }
+
+    const new_request_url = new URL(request.url);
+    if (new_request_url.href.includes("/backpg/")) {
+      return new Response('', { status: 501 });
+    }
+
+    new_request_url.host = env.TARGET_HOST;
+
+    const new_request_headers = new Headers(request.headers);
+    new_request_headers.set('Host', env.TARGET_HOST);
+    new_request_headers.set('Origin', new_request_url.origin);
+    new_request_headers.set('Referer', new_request_url.origin);
+
+    const _response = await fetch(new_request_url.href, {
+        method: request.method,
+        body: request.body,
+        headers: new_request_headers,
+    });
+    const new_response_headers = new Headers(_response.headers);
+    new_response_headers.delete('x-frame-options');
+
+    const new_response = new Response(_response.body, {
+      status: _response.status,
+      headers: new_response_headers
+    });
+
+    if (request.method === "GET" && new_response_headers.get('Content-Type')?.includes('html')) {
+      return new HTMLRewriter()
+        .on('header', elementRemover)
+        .on('footer', elementRemover)
+        .on('script', KeywordRemoverButInZeroCopy(env.TARGET_KEYWORD))
+        .transform(new_response);
+    }
+    return new_response;
+  }
+};
